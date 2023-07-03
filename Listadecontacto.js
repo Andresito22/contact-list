3e0fdf7686d93ef8af5f8373c7575c43784ea491
Andres Sarmiento <andresarmientod@gmail.com>
2023-07-02 22:15:25 -0500

    Ultima entrega

--- Listadecontacto.js
+++ Listadecontacto.js
@@ -22,19 +22,38 @@ agregarContacto("Jaiyelith", "Gonzalez", 105435681, "Bogota", "Tv 20 # 14-03")
 //console.log(listaDeContactos);//
 
 
-function borrarContacto(contacto){
-    const datos = contacto.split("");
-    const nombre = datos[0];
-    const apellido = datos [1];
-    for(let i = 0; i < listaDeContactos.length; i++){
-        if(listaDeContactos[i].nombre === nombre && listaDeContactos[1].apellido === apellido){
-            return listaDeContactos.splice(i, 1);
-        }
+function buscarContacto(contacto) {
+    const [nombre, apellido] = contacto.split(" ");
+    
+    const indiceContacto = listaDeContactos.findIndex(contactFilter => {
+      return contactFilter.nombre === nombre && contactFilter.apellido === apellido;
+    });
+    
+    return indiceContacto !== -1 ? indiceContacto : -1;
+  }
+
+
+  function borrarContacto(contacto) {
+    const indice = buscarContacto(contacto);
+    if (indice == -1){
+        return listaDeContactos;
     }
+    return listaDeContactos.splice(indice, 1);
 }
 /*Para Eliminar un contacto
 borrarContacto("Andres Sarmiento");
 console.log(listaDeContactos);*/
+
+
+
+function actualizarContacto(contacto, datos) {
+    const indice = buscarContacto(contacto);
+    if (indice == -1){
+        return listaDeContactos;
+    }
+    listaDeContactos[indice] = { ...listaDeContactos[indice], ...datos };
+}
+
 
 function imprimirContactos(){
     console.log("Lista de Contactos:"); 
