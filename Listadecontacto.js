1f9347a63bb8f786de3cc12b7eb9fa7675c2b068
Andres Sarmiento <andresarmientod@gmail.com>
2023-07-02 20:39:46 -0500

    Parte 2

--- Listadecontacto.js
+++ Listadecontacto.js
@@ -1,21 +1,39 @@
-let listaDeContactos = ["Andres Sarmiento","Yulieth Blanco","Jaiyelith Gonzalez","Pedro Perez","Julian Casablanca"];
+let listaDeContactos = [];
 
-function agregarContacto(nombre){
-return listaDeContactos.push(nombre);
+function agregarContacto(nombre, apellido, telefono, ciudad, direccion ){
+    const newContact = {
+        id: listaDeContactos.length + 1,
+        nombre,
+        apellido,
+        telefono,
+        ubicaciones:{
+            ciudad,
+            direccion
+        }
+    }
+    return listaDeContactos.push(newContact);
 }
-//Para agregar un contacto//
-/*agregarContacto("Victor Leon");
-console.log(listaDeContactos);*/
 
-function borrarContacto(nombre){
+//Agregando contactos//
+agregarContacto("Andres", "Sarmiento", 123456789, "San Gil", "Cr 9 # 20-20");
+agregarContacto("Yulieth", "Blanco", 202014879, "Bucaramanga", "Calle 61 # 05-10")
+agregarContacto("Jaiyelith", "Gonzalez", 105435681, "Bogota", "Tv 20 # 14-03")
+
+//console.log(listaDeContactos);//
+
+
+function borrarContacto(contacto){
+    const datos = contacto.split("");
+    const nombre = datos[0];
+    const apellido = datos [1];
     for(let i = 0; i < listaDeContactos.length; i++){
-        if(listaDeContactos[i] == nombre){
+        if(listaDeContactos[i].nombre === nombre && listaDeContactos[1].apellido === apellido){
             return listaDeContactos.splice(i, 1);
         }
     }
 }
-//Para Eliminar un contacto//
-/*borrarContacto("Julian Casablanca");
+/*Para Eliminar un contacto
+borrarContacto("Andres Sarmiento");
 console.log(listaDeContactos);*/
 
 function imprimirContactos(){
@@ -24,5 +42,5 @@ function imprimirContactos(){
         console.log(listaDeContactos[i]);
 }
 
-//Imprimir la lista de contactos//
-imprimirContactos(listaDeContactos);
+//Imprimir la lista de contactos
+//imprimirContactos(listaDeContactos);//
